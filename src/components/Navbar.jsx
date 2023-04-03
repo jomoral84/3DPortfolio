2fde46d7b01c615896fc9f53bd89e03bc74c6c24
your name <your@email.com>
2023-04-03 17:40:47 -0300

    Finish navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants/index";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toogle, setToogle] = useState(false);
+
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -18,10 +21,8 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={logo} alt="logo" className="w-3 h-3 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Jorge Morales
-          </p>
+          <img src={logo} alt="logo" className="w-8 h-8 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">Jorge Morales</p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
@@ -34,6 +35,30 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toogle ? close :  menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToogle(!toogle)}
+          />
+          <div className={`${!toogle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+
+          <ul className="list-none flex justify-end items-start flex-col gap-4">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={` hover: text-white text-[16px] font-poppins font-medium cursor-pointer`}
+              onClick={() => {
+                setToogle(!toogle);
+                setActive(link.title);}}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
