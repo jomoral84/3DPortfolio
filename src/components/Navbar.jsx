36ab57fcd6d5a62f2832aae846c41b10667a29d9
Jor <you@example.com>
2023-06-15 16:39:45 -0300

    Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-8 h-8 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">Jorge Morales</p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Jorge Morales
+          </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
@@ -37,26 +39,32 @@ const Navbar = () => {
         </ul>
         <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
-            src={toogle ? close :  menu}
+            src={toogle ? close : menu}
             alt="menu"
             className="w-[28px] h-[28px] object-contain cursor-pointer"
             onClick={() => setToogle(!toogle)}
           />
-          <div className={`${!toogle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-
-          <ul className="list-none flex justify-end items-start flex-col gap-4">
-          {navLinks.map((link) => (
-            <li
-              key={link.id}
-              className={` hover: text-white text-[16px] font-poppins font-medium cursor-pointer`}
-              onClick={() => {
-                setToogle(!toogle);
-                setActive(link.title);}}
-            >
-              <a href={`#${link.id}`}>{link.title}</a>
-            </li>
-          ))}
-        </ul>
+          <div
+            className={`${
+              !toogle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === link.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToogle(!toogle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
